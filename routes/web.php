bc0678545935a3756c20932fa0fc622f127a6d05
Александр <iumag@yandex.ru>
2017-01-08 14:57:59 +0800

    Admin Authorization Add

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['middleware' => ['web']], function() {
+
+// Login Routes...
+    Route::get('admin', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+    Route::post('admin', ['as' => 'login.post', 'uses' => 'Auth\LoginController@login']);
+    Route::post('admin/logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+// Registration Routes...
+    Route::get('admin/register', ['as' => 'register', 'uses' => 'Auth\RegisterController@showRegistrationForm']);
+    Route::post('admin/register', ['as' => 'register.post', 'uses' => 'Auth\RegisterController@register']);
+
+// Password Reset Routes...
+    Route::get('admin/password/reset', ['as' => 'password.reset', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
+    Route::post('admin/password/email', ['as' => 'password.email', 'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail']);
+    Route::get('admin/password/reset/{token}', ['as' => 'password.reset.token', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+    Route::post('admin/password/reset', ['as' => 'password.reset.post', 'uses' => 'Auth\ResetPasswordController@reset']);
+});
+
+
+Route::get('/admin/index', 'AdminController@index');
