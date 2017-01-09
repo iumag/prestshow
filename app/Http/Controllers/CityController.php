20483ec4e2d20f2ccbfc7d7015b35a575bdd58bb
iumag <iumag@yandex.ru>
2017-01-09 16:51:52 +0800

    CityController

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -7,12 +7,88 @@ use App\City;
 
 class CityController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return response()
             ->json([
                 'model' => City::FilterPaginateOrder()
             ]);
     }
 
-    
+    public function create()
+    {
+        return response()
+            ->json([
+                'form' => City::initalize(),
+                'option' => []
+            ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'city' => 'required',
+            'picture' => 'required',
+            'description' => 'required',
+            'cost' => 'required|numeric|min:0',
+        ]);
+
+        $city = City::create($request->all());
+
+        return response()
+            ->json([
+                'saved' => true
+            ]);
+    }
+
+    public function show($id)
+    {
+        $city = City::findOrFail($id);
+
+        return response()
+            ->json([
+                'model' => $city
+            ]);
+    }
+
+    public function edit($id)
+    {
+        $city = City::findOrFail($id);
+
+        return response()
+            ->json([
+                'model' => $city,
+                'option' => ''
+            ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'city' => 'required',
+            'picture' => 'required',
+            'description' => 'required',
+            'cost' => 'required|numeric|min:0',
+        ]);
+
+        $city = City::findOrFail($id);
+        $city->update($request->all());
+
+        return response()
+            ->json([
+                'saved' => true
+            ]);
+    }
+
+    public function destroy($id)
+    {
+        $city = City::findOrFail($id);
+
+        $city->delete();
+
+        return response()
+            ->json([
+                'saved' => true
+            ]);
+    }
 }
