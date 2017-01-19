3b4eb3d15aae8fd5d3dca4652c0b9ce2c8a897e8
Александр <iumag@yandex.ru>
2017-01-19 23:21:12 +0800

    transport get data

--- resources/assets/js/views/site/transport.vue
+++ resources/assets/js/views/site/transport.vue
@@ -3,80 +3,61 @@
         <div class="browser_width grpelem" id="u16487-bw">
             <div id="u16487"><!-- simple frame --></div>
         </div>
-        <div class="PamphletWidget clearfix widget_invisible grpelem" id="pamphletu17383"><!-- none box -->
-            <div class="ThumbGroup clearfix grpelem" id="u17403"><!-- none box -->
-                <div class="popup_anchor">
-                    <div class="Thumb popup_element rounded-corners clearfix" id="u17404"><!-- group -->
-                        <div class="clip_frame grpelem" id="u17405"><!-- image -->
-                            <img class="block" id="u17405_img" src="images/%d1%81%d0%bc%d1%87%d1%87%d1%81%d0%bc%d1%81%d0%bc.png" alt="" width="43" height="57"/>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="popup_anchor" id="u17388popup">
-                <div class="ContainerGroup rgba-background clearfix" id="u17388"><!-- stack box -->
-                    <div class="Container rounded-corners clearfix grpelem" id="u17389"><!-- column -->
-                        <div class="clearfix colelem" id="pu17398-4"><!-- group -->
-                            <div class="clearfix grpelem" id="u17398-4"><!-- content -->
-                                <p>Limuzyna</p>
-                            </div>
-                            <div class="clearfix grpelem" id="u17393-4"><!-- content -->
-                                <p>100 zl</p>
-                            </div>
-                            <div class="PamphletWidget clearfix grpelem" id="pamphletu18368"><!-- none box -->
-                                <div class="ThumbGroup clearfix grpelem" id="u18371"><!-- none box -->
-                                    <div class="popup_anchor">
-                                        <div class="Thumb popup_element clearfix" id="u18372"><!-- group -->
-                                            <div class="clip_frame grpelem" id="u18373"><!-- image -->
-                                                <img class="block" id="u18373_img" src="images/shopping-cart%20(1)2.png" alt="" width="32" height="32"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="popup_anchor" id="u18377popup">
-                                    <div class="ContainerGroup clearfix" id="u18377"><!-- none box -->
-                                        <div class="Container invi clearfix grpelem" id="u18378"><!-- group -->
-                                            <div class="clip_frame grpelem" id="u18379"><!-- image -->
-                                                <img class="block" id="u18379_img" src="images/shopping-cart-verified-symbol.png" alt="" width="32" height="32"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="clearfix colelem" id="u17392-7"><!-- content -->
-                            <p>Limuzyna – luksusowy samochód, najczęściej spotykany w kolorze czarnym lub białym.</p>
-                            <p>&nbsp;</p>
-                            <p>Nadwozie zamknięte, czterodrzwiowe. Klasyczny układ napędowy. Może posiadać od czterech do kilkunastu miejsc siedzących, czasem dwa lub trzy dodatkowe składane siedzenia. Siedzenie kierowcy i pasażera zazwyczaj są oddzielone od tylnej części kabiny (siedzenia &quot;VIP&quot;) szybą lub ścianką - wtedy nazywamy to samochodem dual cowl. Mianem limuzyny określić można również samochód luksusowy o nadwoziu typu sedan lub hatchback, takim jak np.</p>
-                        </div>
-                        <div class="clearfix colelem" id="pu17399"><!-- group -->
-                            <div class="clip_frame clearfix grpelem" id="u17399"><!-- image -->
-                                <img class="position_content" id="u17399_img" src="images/750x400_rozowe-limuzyny-warszawa.jpg" alt="" width="180" height="96"/>
-                            </div>
-                            <div class="clip_frame clearfix grpelem" id="u17394"><!-- image -->
-                                <img class="position_content" id="u17394_img" src="images/cadillac-limuzyna.jpg" alt="" width="158" height="99"/>
-                            </div>
-                            <div class="clip_frame clearfix grpelem" id="u17396"><!-- image -->
-                                <img class="position_content" id="u17396_img" src="images/new_chrysler_300_warsaw1.jpg" alt="" width="158" height="105"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="popup_anchor">
-                <div class="PamphletCloseButton PamphletLightboxPart popup_element clearfix" id="u17386"><!-- group -->
-                    <div class="clearfix grpelem" id="u17387-4"><!-- content -->
-                        <p>x</p>
-                    </div>
-                </div>
+        <div class="wrap" style="
+    margin-top: 335px;
+">
+            <div class="transportwrap" v-for="item in model.data">
+                <a class="nonblock nontext anim_swing clearfix grpelem" id="u17814-4" href="index.html#o-transporcie">
+                    <!-- content --><p>{{item.name}}</p></a>
+                <a class="nonblock nontext anim_swing rounded-corners gradient clip_frame clearfix grpelem" id="fototr"
+                   href="index.html#o-transporcie"><!-- image --><img class="position_content" id="u17826_img"
+                                                                      :src="'img/transport/'+item.picture" alt="" width="305"
+                                                                      height="202"/></a>
             </div>
         </div>
-        <a class="nonblock nontext anim_swing rounded-corners gradient clip_frame clearfix grpelem" id="u17579" href="index.html#cena"><!-- image --><img class="position_content" id="u17579_img" src="images/untidcheck.jpg" alt="" width="305" height="203"/></a>
-        <a class="nonblock nontext anim_swing rounded-corners gradient clip_frame clearfix grpelem" id="u17699" href="index.html#cena"><!-- image --><img class="position_content" id="u17699_img" src="images/showcase.jpg" alt="" width="305" height="210"/></a>
-        <a class="nonblock nontext anim_swing rounded-corners gradient clip_frame clearfix grpelem" id="u17419" href="index.html#cena"><!-- image --><img class="position_content" id="u17419_img" src="images/new_chrysler_300_warsaw1.jpg" alt="" width="305" height="203"/></a>
-        <a class="nonblock nontext anim_swing clearfix grpelem" id="u17407-4" href="index.html#cena"><!-- content --><p>Limuzyna</p></a>
-        <a class="nonblock nontext anim_swing clearfix grpelem" id="u17567-4" href="index.html#cena"><!-- content --><p>Taxi</p></a>
-        <a class="nonblock nontext anim_swing clearfix grpelem" id="u17687-4" href="index.html#cena"><!-- content --><p>Auto</p></a>
         <a class="anchor_item grpelem" id="typy-transportu"></a>
     </div>
 </template>
+
+<script>
+    import axios from 'axios'
+    import Vue from 'vue'
+    export default {
+        data() {
+            return {
+                model: {
+                    data: []
+                },
+                params: {
+                    column: 'id',
+                    direction: 'desc',
+                    per_page: 10,
+                    page: 1,
+                    search_column: 'id',
+                    search_operator: 'equal_to',
+                    search_query_1: '',
+                    search_query_2: ''
+                },
+            }
+        },
+        beforeMount() {
+            this.fetchData()
+        },
+        methods: {
+            fetchData() {
+                var vm = this
+                axios.get(this.buildURL())
+                    .then(function(response){
+                        Vue.set(vm.$data, 'model', response.data.model)
+                    })
+                    .catch(function(error){
+                        console.log(error)
+                    })
+            },
+            buildURL() {
+                var p = this.params
+                return `/api/transport?column=${p.column}&direction=${p.direction}&page=${p.page}&search_column=${p.search_column}&search_operator=${p.search_operator}&search_query_1=${p.search_query_1}&search_query_2=${p.search_query_2}`
+            }
+        }
+    }
+</script>
