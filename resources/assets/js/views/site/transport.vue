f6e1a368ec341e9461ec32ff05942c34b690e09f
Александр <iumag@yandex.ru>
2017-01-22 19:23:14 +0800

    Transport computed Cost

--- resources/assets/js/views/site/transport.vue
+++ resources/assets/js/views/site/transport.vue
@@ -9,9 +9,11 @@
             <div class="transportwrap" v-for="item in model.data">
                 <a class="nonblock nontext anim_swing clearfix grpelem" id="u17814-4" href="index.html#o-transporcie">
                     <!-- content --><p>{{item.name}}</p></a>
-                <a class="nonblock nontext anim_swing rounded-corners gradient clip_frame clearfix grpelem" id="fototr"
+                <a @click="item.show = !item.show"
+                   class="nonblock nontext anim_swing rounded-corners gradient clip_frame clearfix grpelem" id="fototr"
                    href="index.html#o-transporcie"><!-- image --><img class="position_content" id="u17826_img"
-                                                                      :src="'img/transport/'+item.picture" alt="" width="305"
+                                                                      :src="'img/transport/'+item.picture" alt=""
+                                                                      width="305"
                                                                       height="202"/></a>
             </div>
         </div>
@@ -47,16 +49,26 @@
             fetchData() {
                 var vm = this
                 axios.get(this.buildURL())
-                    .then(function(response){
+                    .then(function (response) {
                         Vue.set(vm.$data, 'model', response.data.model)
                     })
-                    .catch(function(error){
+                    .catch(function (error) {
                         console.log(error)
                     })
             },
             buildURL() {
                 var p = this.params
                 return `/api/transport?column=${p.column}&direction=${p.direction}&page=${p.page}&search_column=${p.search_column}&search_operator=${p.search_operator}&search_query_1=${p.search_query_1}&search_query_2=${p.search_query_2}`
+            },
+            Total(){
+                var result = this.model.data.reduce(function (carry, item) {
+                    if (item.show) {
+                        carry += parseFloat(item.cost)
+                    }
+                    return carry
+                }, 0)
+                this.total = result
+                console.log(this.total)
             }
         }
     }
